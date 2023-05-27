b610855d6b6c0aafa35adc436e047176fe6d5d6b
TodorNik <122238741+TodorNik@users.noreply.github.com>
2023-05-27 02:06:06 +0200

    Update enemy.lua

--- enemy.lua
+++ enemy.lua
@@ -1,6 +1,7 @@
 Enemy = Object:extend()
 
 function Enemy:new(x, y, z)
+--Takes args for location and speed of the enemy and creates it
     self.image = love.graphics.newImage("snake.png")
     local window_width = love.graphics.getWidth()
     self.x = x
@@ -10,10 +11,12 @@ function Enemy:new(x, y, z)
     self.height = self.image:getHeight()
 end
 
+--Moves the enemy
 function Enemy:update(dt)
     self.x = self.x - self.speed * dt
 end
 
+--Draws the enemy on the canvas
 function Enemy:draw()
     love.graphics.draw(self.image, self.x, self.y)
 end
